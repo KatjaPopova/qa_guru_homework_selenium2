1772e3c3b12cbed74dd95301e3170631f5a95d34
KatjaPopova <popovaea@tutu.ru>
2026-08-11 13:35:11 +0300

    Deleted useless imports

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,6 @@
 import pytest
 from selenium import webdriver
 
-from pages.login_page import LoginPage
-from pages.tables_page import TablesPage
-from pages.text_box_page import TextBoxPage
-
 
 @pytest.fixture()
 def driver():
